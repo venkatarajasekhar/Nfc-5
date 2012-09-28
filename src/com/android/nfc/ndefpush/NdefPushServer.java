86f0501a8ebfda6ab550825126c5e86a169c5a87
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-09-28 14:07:48 +0800

    Fix thread leakage
    When server socket closed, the while loop for incoming socket
    should be terminated immediately.
    
    Also fix a race condition in NdefPushServer to avoid null pointer
    exception on shutdown. The issue was hidden due to the thread
    leakage issue. Fortunately SnepServer doesn't have the issue.
    
    Don't show IOException thrown from serverSocket.accept() as an
    error since it's expected.
    
    Change-Id: Ie5cfb15dd6b2d899dde83e5c613d85d0cc8fecb2
    
    Conflicts:
    
    	src/com/android/nfc/ndefpush/NdefPushServer.java
    
    	modified:   nxp/jni/com_android_nfc.h
    	modified:   nxp/jni/com_android_nfc_NativeLlcpServiceSocket.cpp
    	modified:   src/com/android/nfc/ndefpush/NdefPushServer.java
    	modified:   src/com/android/nfc/snep/SnepServer.java

--- src/com/android/nfc/ndefpush/NdefPushServer.java
+++ src/com/android/nfc/ndefpush/NdefPushServer.java
@@ -150,6 +150,24 @@ public class NdefPushServer {
                         }
                         if (serverSocket == null) return;
 
+                    synchronized (this) {
+                        mServerSocket = mService.createLlcpServerSocket(mSap, SERVICE_NAME,
+                                MIU, 1, 1024);
+                        if (mServerSocket == null) {
+                            if (DBG) Log.d(TAG, "failed to create LLCP service socket");
+                            return;
+                        }
+                    }
+                    if (DBG) Log.d(TAG, "created LLCP service socket");
+                    while (mRunning) {
+                        LlcpServerSocket serverSocket;
+                        synchronized (this) {
+                            serverSocket = mServerSocket;
+                        }
+                        if (serverSocket == null) {
+                            if (DBG) Log.d(TAG, "Server socket shut down.");
+                            return;
+                        }
                         if (DBG) Log.d(TAG, "about to accept");
                         LlcpSocket communicationSocket = serverSocket.accept();
                         if (DBG) Log.d(TAG, "accept returned " + communicationSocket);
@@ -165,9 +183,10 @@ public class NdefPushServer {
                 } catch (LlcpException e) {
                     Log.e(TAG, "llcp error", e);
                 } catch (IOException e) {
-                    Log.e(TAG, "IO error", e);
+                    if (DBG) Log.d(TAG, "IO error");
                 } finally {
                     synchronized (NdefPushServer.this) {
+                    synchronized (this) {
                         if (mServerSocket != null) {
                             if (DBG) Log.d(TAG, "about to close");
                             try {
@@ -196,6 +215,13 @@ public class NdefPushServer {
                         // ignore
                     }
                     mServerSocket = null;
+        public synchronized void shutdown() {
+            mRunning = false;
+            if (mServerSocket != null) {
+                try {
+                    mServerSocket.close();
+                } catch (IOException e) {
+                    // ignore
                 }
             }
         }
